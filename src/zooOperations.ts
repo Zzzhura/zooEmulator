1f218ec06ac1115cc7e9436be36ef65951bd188e
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-08-03 21:43:06 +0300

    added new function that will compare animal and cage props

--- src/zooOperations.ts
+++ src/zooOperations.ts
@@ -1,12 +1,7 @@
 import { Animal, Cage, Zoo } from "./TaskInterfaces";
 
 export function addAnimalToCage(animal: Animal, cage: Cage) {
-  if (
-    cage.square - animal.neededSquare >= 0 &&
-    cage.biom == animal.neededBiom &&
-    cage.hasReservoir == animal.needOfReservoir &&
-    cage.isForPredators == animal.isPredator
-  ) {
+  if (compareAnimalToCageProps(animal, cage)) {
     cage.members.push(animal);
     cage.square = cage.square - animal.neededSquare;
     console.log(animal.name + "added to cage");
@@ -18,10 +13,23 @@ export function addAnimalToCage(animal: Animal, cage: Cage) {
   }
 }
 
+export function compareAnimalToCageProps(animal: Animal, cage: Cage) {
+  if (
+    cage.square - animal.neededSquare >= 0 &&
+    cage.biom == animal.neededBiom &&
+    cage.hasReservoir == animal.needOfReservoir &&
+    cage.isForPredators == animal.isPredator
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export function calculateAmountOfFoodForCage(cage: Cage) {
   let amountOfFood: number = 0;
   if (cage.members.length == 0) {
-    console.log('Cage is empty')
+    console.log("Cage is empty");
     return amountOfFood;
   } else {
     for (let i = 0; i < cage.members.length; i++) {
@@ -30,7 +38,6 @@ export function calculateAmountOfFoodForCage(cage: Cage) {
   }
   return amountOfFood;
 }
- 
 
 export function isAnimalHasNeededType(animal: Animal, cage: Cage) {
   return animal.isPredator == cage.isForPredators ? true : false;
